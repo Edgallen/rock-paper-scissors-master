36952844bf0b101031d34b6d8f8133bd4f501b94
Edgallen <82937338+Edgallen@users.noreply.github.com>
2022-03-15 14:14:53 +0000

    Changed html class names to easier navigation
    Added resultsWindow
    
    Added script to change selectorsWindow to resultsWindow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 /*========== FUNCTIONS ==========*/
-const closeWindow = (element) => {
+const closeWindow = (element, delay) => {
   element.classList.add('opacity-hidden');
 
-  setTimeout(() => element.classList.add('display-none'), 450);
+  setTimeout(() => element.classList.add('display-none'), delay);
 }
 
-const openWindow = (element) => {
+const openWindow = (element, delay) => {
   element.classList.remove('display-none');
 
-  setTimeout(() => element.classList.remove('opacity-hidden'), 10);
+  setTimeout(() => element.classList.remove('opacity-hidden'), delay);
 }
 
 /*========== MODAL WINDOW ==========*/
@@ -18,12 +18,40 @@ const modalWindow = document.querySelector('.game__modal');
 
 rulesButton.addEventListener('click', () => {
   if(modalWindow.classList.contains('display-none')){
-    openWindow(modalWindow);
+    openWindow(modalWindow, 10);
   } else {
-    closeWindow(modalWindow);
+    closeWindow(modalWindow, 450);
   }
 });
 
 closeButton.addEventListener('click', () => {
   closeWindow(modalWindow);
 })
+
+/*========== GAME ==========*/
+const selectorsWindow = document.querySelector('.game__container-selectorsWindow');
+const resultsWindow = document.querySelector('.game__container-resultsWindow');
+const selectorButtons = document.querySelectorAll('.game__selector');
+const selectors = [
+  {
+    name: 'rock',
+    beats:'scissors'
+  },
+
+  {
+    name: 'paper',
+    beats:'rock'
+  },
+
+  {
+    name: 'scissors',
+    beats:'paper'
+  }
+];
+
+selectorButtons.forEach(element => {
+  element.addEventListener('click', () => {
+    closeWindow(selectorsWindow, 210);
+    setTimeout(() => openWindow(resultsWindow, 10), 210);
+  });
+});
